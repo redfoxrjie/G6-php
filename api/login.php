0c98fff8b5a812cd46233a4ae127e066235af9f8
kljenny <jury10271995@gmail.com>
2024-07-11 16:21:08 +0800

    feat:增加會員管理php

--- api/login.php
+++ api/login.php
@@ -1,5 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Content-Type: application/json");
--- api/register.php
+++ api/register.php
@@ -1,5 +1,4 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
 header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With");
 header("Content-Type: application/json");
